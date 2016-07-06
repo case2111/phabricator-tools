1311ad5433a8cf98ed6e33d57d963fa5068ceb52
Sean Enck <enckse@gmail.com>
2016-07-06 16:56:34 -0400

    Cleaning up output

--- utilities/jenkins2conpherence/jobs_by_color.py
+++ utilities/jenkins2conpherence/jobs_by_color.py
@@ -19,10 +19,18 @@ def main():
     server = jenkins.Jenkins(args.host, username=args.user, password=args.pwd)
     colors = args.colors.split(',')
     do_all = args.colors == ALL_COLORS
+    color_out = {}
     for job in server.get_jobs():
         job_color = job['color']
         if job_color in colors or do_all:
-            print("{0} is currently {1}".format(job['name'], job_color))
+            if job_color not in color_out:
+                color_out[job_color] = []
+            color_out[job_color].append(job['name'])
+    for key in color_out:
+        print(key)
+        for item in color_out[key]:
+            print('\t{0}'.format(item))
+        print('\n')
 
 if __name__ == '__main__':
     main()
