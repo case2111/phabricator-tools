54ecf613e91f0052e257f42986a87dc440e3e959
Sean Enck <enckse@gmail.com>
2016-07-06 16:47:27 -0400

    Adding an 'all' option for colors

--- utilities/jenkins2conpherence/jobs_by_color.py
+++ utilities/jenkins2conpherence/jobs_by_color.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python
+
+"""Retrieve and output jobs by color."""
+
 import jenkins
 import argparse
 
+ALL_COLORS = 'ALL'
+
+
 def main():
+    """Main entry."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', required=True, type=str)
     parser.add_argument('--user', required=True, type=str)
@@ -10,9 +18,10 @@ def main():
     args = parser.parse_args()
     server = jenkins.Jenkins(args.host, username=args.user, password=args.pwd)
     colors = args.colors.split(',')
+    do_all = args.colors == ALL_COLORS
     for job in server.get_jobs():
         job_color = job['color']
-        if job_color in colors:
+        if job_color in colors or do_all:
             print("{0} is currently {1}".format(job['name'], job_color))
 
 if __name__ == '__main__':
