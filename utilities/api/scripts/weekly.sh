b8927648eb336a50ced810f82965456c2bcf7f26
Sean Enck <enckse@users.noreply.github.com>
2016-11-18 17:39:34 -0500

    Changing variable name

--- utilities/api/scripts/weekly.sh
+++ utilities/api/scripts/weekly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LOCATION=/opt/phabricator-tools/utilities/api/
-python ${LOCATION}duedates.py --host $PHAB_HOST --token $PHAB_TOKEN --room $COMMON_ROOM
-python ${LOCATION}unmodified.py --host $PHAB_HOST --token $PHAB_TOKEN --room $COMMON_ROOM --report 30 --close 45
+python ${LOCATION}duedates.py --host $PHAB_HOST --token $PHAB_TOKEN --room $PHAB_COMMON_ROOM
+python ${LOCATION}unmodified.py --host $PHAB_HOST --token $PHAB_TOKEN --room $PHAB_COMMON_ROOM --report 30 --close 45
