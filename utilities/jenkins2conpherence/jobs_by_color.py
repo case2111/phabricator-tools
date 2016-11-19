a28c662a4da45a40c268e3ba4ac4a0419948f0bc
Sean Enck <enckse@gmail.com>
2016-11-19 09:05:09 -0500

    Converting jenkins over to use conduit api

--- utilities/jenkins2conpherence/jobs_by_color.py
+++ utilities/jenkins2conpherence/jobs_by_color.py
@@ -5,6 +5,7 @@
 import jenkins
 import argparse
 import re
+import conduit
 
 ALL_COLORS = 'ALL'
 
@@ -12,13 +13,16 @@ ALL_COLORS = 'ALL'
 def main():
     """Main entry."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', required=True, type=str)
+    parser.add_argument('--jenkins', required=True, type=str)
     parser.add_argument('--user', required=True, type=str)
     parser.add_argument('--pwd', required=True, type=str)
     parser.add_argument('--colors', required=True, type=str)
     parser.add_argument('--ignore', default=None, type=str)
+    parser.add_argument("--host", required=True, type=str)
+    parser.add_argument("--token", required=True, type=str)
+    parser.add_argument("--room", required=True, type=int)
     args = parser.parse_args()
-    server = jenkins.Jenkins(args.host, username=args.user, password=args.pwd)
+    server = jenkins.Jenkins(args.jenkins, username=args.user, password=args.pwd)
     colors = args.colors.split(',')
     do_all = args.colors == ALL_COLORS
     color_out = {}
@@ -37,12 +41,17 @@ def main():
             if job_color not in color_out:
                 color_out[job_color] = []
             color_out[job_color].append(job_name)
+    messages = []
     for key in sorted(color_out.keys()):
-        print(key)
-        print('---')
+        messages.append(key)
+        messages.append('---')
         for item in sorted(color_out[key]):
-            print('{0}'.format(item))
-        print('\n')
+            messages.append('{0}'.format(item))
+        messages.append('\n')
+    c = conduit.Conpherence()
+    c.host = args.host
+    c.token = args.token
+    c.updatethread(args.room, "\n".join(messages))
 
 if __name__ == '__main__':
     main()
--- utilities/jenkins2conpherence/post.sh
+++
@@ -1,5 +0,0 @@
-status=$(cat $3)
-if [ -z "$status" ]; then
-    exit 0
-fi
-curl $1/api/conpherence.updatethread -d api.token=$2 -d message="$status" -d id=$4 --connect-timeout $5
