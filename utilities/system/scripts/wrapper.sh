8e9d5a986c7727fc77e7d3cf0efab578b6601e33
Sean Enck <enckse@gmail.com>
2016-11-21 12:59:35 -0500

    More comphrensive support

--- utilities/system/scripts/wrapper.sh
+++ utilities/system/scripts/wrapper.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 source /etc/environment
-LOCATION=/opt/phabricator-tools/utilities/tasks/
+LOCATION=/opt/phabricator-tools/utilities/
+TASKS="tasks"
+CALENDAR="calendar"
+
+function run-item()
+{
+    python ${LOCATION}$1 --host $PHAB_HOST --token $PHAB_TOKEN $2
+}
+
 function do-duedates()
 {
-    python ${LOCATION}duedates.py --host $PHAB_HOST --token $PHAB_TOKEN --mode $1
+    run-item "$TASKS/duedates.py" "--mode $1"
 }
+
 case $1 in
     "weekly")
     do-duedates 1
-    python ${LOCATION}unmodified.py --host $PHAB_HOST --token $PHAB_TOKEN --room $PHAB_COMMON_ROOM --report 30 --close 45
+    run-item "$TASKS/unmodified.py" "--room $PHAB_COMMON_ROOM --report 30 --close 45"
     ;;
     "daily")
     do-duedates 2
+    run-item "$CALENDAR/today.py" "--room $PHAB_BOT_ROOM"
     ;;
 esac
