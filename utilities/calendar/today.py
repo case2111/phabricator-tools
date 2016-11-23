855c55deef54061d90dcf3d063e9a84d8202dd2e
Sean Enck <enckse@gmail.com>
2016-11-23 09:11:16 -0500

    styling (pep8(

--- utilities/calendar/today.py
+++ utilities/calendar/today.py
@@ -6,6 +6,7 @@ import conduit
 import time
 import calendar
 
+
 def _process(host, token, room):
     """Process event dates."""
     factory = conduit.Factory()
@@ -23,9 +24,13 @@ def _process(host, token, room):
         start = fields["startDateTime"]["epoch"]
         end = fields["endDateTime"]["epoch"]
         start_time = time.gmtime(start)
-        is_all = fields['isAllDay'] and  start_time.tm_mon == gm.tm_mon and start_time.tm_mday == gm.tm_mday and start_time.tm_year == gm.tm_year
+        is_all = fields['isAllDay'] and \
+            start_time.tm_mon == gm.tm_mon and \
+            start_time.tm_mday == gm.tm_mday and \
+            start_time.tm_year == gm.tm_year
         if (start >= now and end <= now_end) or is_all:
-            msg = "E{0} is today (flagged all day? {1})".format(item["id"], is_all)
+            msg = "E{0} is today (flagged all day? {1})".format(item["id"],
+                                                                is_all)
             conph.updatethread(room, msg)
 
 
@@ -36,7 +41,7 @@ def main():
     parser.add_argument("--token", required=True, type=str)
     parser.add_argument("--room", required=True, type=str)
     args = parser.parse_args()
-    _process(args.host, args.token, args.room) 
+    _process(args.host, args.token, args.room)
 
 
 if __name__ == '__main__':
