52d07a449fa26f934050c13ac2662174e5efea91
Sean Enck <enckse@gmail.com>
2016-11-23 09:06:13 -0500

    adjustments to support all day events

--- utilities/calendar/today.py
+++ utilities/calendar/today.py
@@ -14,15 +14,18 @@ def _process(host, token, room):
     c = factory.create(conduit.CalendarEvent)
     who = factory.create(conduit.User).whoami()["phid"]
     e = c.upcoming_by_subscriber(who)["data"]
-    now = calendar.timegm(time.localtime())
+    gm = time.gmtime()
+    now = calendar.timegm(gm)
     now_end = now + 86400
     conph = factory.create(conduit.Conpherence)
     for item in e:
         fields = item["fields"]
         start = fields["startDateTime"]["epoch"]
         end = fields["endDateTime"]["epoch"]
-        if start >= now and end <= now_end:
-            msg = "E{0} is today".format(item["id"])
+        start_time = time.gmtime(start)
+        is_all = fields['isAllDay'] and  start_time.tm_mon == gm.tm_mon and start_time.tm_mday == gm.tm_mday and start_time.tm_year == gm.tm_year
+        if (start >= now and end <= now_end) or is_all:
+            msg = "E{0} is today (flagged all day? {1})".format(item["id"], is_all)
             conph.updatethread(room, msg)
 
 
