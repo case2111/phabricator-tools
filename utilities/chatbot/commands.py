22ede4f228931ae570af64d78f936bc725d8ed2b
Sean Enck <enckse@gmail.com>
2016-11-30 10:17:59 -0500

    cleaning up debug implementation

--- utilities/chatbot/commands.py
+++ utilities/chatbot/commands.py
@@ -8,6 +8,7 @@ DEBUG_CMD = "debug"
 DEBUG_CMDS = [ECHO_CMD, CHAT_CMD, DEBUG_CMD]
 ALL_CMDS = [HELP_CMD]
 
+
 class Context(object):
     """Context for executing commands."""
 
@@ -45,28 +46,43 @@ def _updatethread(ctx, room_id, msg):
     ctx.get(Context.CONPH).updatethread(room_id, msg)
 
 
+def _create_chatbot():
+    """Create a chatbot."""
+    from chatterbot import ChatBot
+    chat = ChatBot("chatbot",
+                   trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+    chat.train("chatterbot.corpus.english")
+    return chat
+
+
 def execute(command, parameters, room_id, ctx, debugging):
     """Execute a command."""
-    debug = ctx.get(Context.DEBUG)
-    if command == ECHO_CMD and debug:
-        _updatethread(ctx, room_id, " ".join(parameters))
-    elif command == CHAT_CMD and debug:
-        from chatterbot import ChatBot
-        chatty = ctx.get(Context.CHATBOT)
-        if chatty is None:
-            chatty = ChatBot("chatbot", trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
-            chatty.train("chatterbot.corpus.english")
-            ctx.set(Context.CHATBOT, chatty)
-        if len(parameters) == 0:
-            resp = "/silence"
+    try:
+        cmd = command
+        debug = ctx.get(Context.DEBUG)
+        if not debug and command in DEBUG_CMDS:
+            cmd = HELP_CMD
+        if cmd == ECHO_CMD:
+            _updatethread(ctx, room_id, " ".join(parameters))
+        elif cmd == CHAT_CMD:
+            chatty = ctx.get(Context.CHATBOT)
+            if chatty is None:
+                chatty = _create_chatbot()
+                ctx.set(Context.CHATBOT, chatty)
+            if len(parameters) == 0:
+                resp = "/silence"
+            else:
+                resp = chatty.get_response(" ".join(parameters))
+            _updatethread(ctx, room_id, str(resp))
+        elif command == DEBUG_CMD and debugging:
+            ctx.set(Context.DEBUG, not debug)
+            _updatethread(ctx, room_id, "debug mode toggled")
         else:
-            resp = chatty.get_response(" ".join(parameters))
-        _updatethread(ctx, room_id, str(resp))
-    elif command == DEBUG_CMD and debugging:
-        ctx.set(Context.DEBUG, not debug)
-        _updatethread(ctx, room_id, "debug mode toggled")
-    else:
-        use_cmds = ALL_CMDS
-        if debug:
-            use_cmds += DEBUG_CMDS
-        _updatethread(ctx, room_id, "available: " + ",".join(sorted(use_cmds)))
+            use_cmds = ALL_CMDS
+            if debug:
+                use_cmds += DEBUG_CMDS
+            _updatethread(ctx,
+                          room_id,
+                          "available: " + ",".join(sorted(use_cmds)))
+    except Exception as e:
+        _updatethread(ctx, room_id, str(e))
