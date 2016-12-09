07449dce10f066ed8dbc51228e49c0f0968ac923
Sean Enck <enckse@gmail.com>
2016-12-09 11:48:49 -0500

    always do this

--- utilities/system/scripts/timed.sh
+++ utilities/system/scripts/timed.sh
@@ -4,6 +4,4 @@ curl -s $PHAB_HOST/api/conpherence.updatethread \
     -d api.token="$PHAB_STATUS_TOKEN" \
     -d id=$PHAB_BOT_ROOM \
     -d message="$PHAB_TIMED_MSG $1"
-if [[ $1 == "daily" ]]; then
-    rm -f /opt/phab/static/*
-fi
+rm -f /opt/phab/static/*
