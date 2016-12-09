458b1f2af7dbd4ba5ab6b21fb6d4a7e6a09dc76d
Sean Enck <enckse@gmail.com>
2016-12-09 08:13:30 -0500

    switching to using bot chat room

--- utilities/system/scripts/timed.sh
+++ utilities/system/scripts/timed.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /etc/environment
-python ${PHAB_TOOLS}/utilities/src/task_main.py --mode $1
+curl -s $PHAB_HOST/api/conpherence.updatethread \
+    -d api.token="$PHAB_STATUS_TOKEN" \
+    -d id=$PHAB_BOT_ROOM \
+    -d message="$PHAB_TIMED_MSG $1"
 if [[ $1 == "daily" ]]; then
     rm -f /opt/phab/static/*
 fi
