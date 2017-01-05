b3366fb12581e6bea7c544c263ddb0985b056c1c
Sean Enck <enckse@gmail.com>
2017-01-05 11:33:48 -0500

    update from prune -> schedule

--- utilities/system/scripts/chat.sh
+++ utilities/system/scripts/chat.sh
@@ -2,13 +2,13 @@
 source /etc/environment
 LOCK_FILE=/tmp/chatbot.lck.
 PHAB_SRC=$PHAB_TOOLS/utilities/src/
-BOTS="$PHAB_MON_TOKEN|monitor $PHAB_TASK_TOKEN|prune"
+BOTS="$PHAB_MON_TOKEN|monitor $PHAB_TASK_TOKEN|schedule"
 
 # start the chatbot
 function start-now()
 {
     git --git-dir $PHAB_TOOLS/.git log -n 1 | grep "^commit" > ${PHAB_TOOLS}/version.txt
-    python -u ${PHAB_SRC}chat_bot.py --host $PHAB_HOST --last 30 --lock $LOCK_FILE --monitor $PHAB_MON_TOKEN --prune $PHAB_TASK_TOKEN
+    python -u ${PHAB_SRC}chat_bot.py --host $PHAB_HOST --last 30 --lock $LOCK_FILE --monitor $PHAB_MON_TOKEN --schedule $PHAB_TASK_TOKEN
 }
 
 # stop the chatbot
