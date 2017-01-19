9dd9fb15ae2886987c16b554ab838eb21705c92a
Sean Enck <enckse@gmail.com>
2017-01-19 11:09:01 -0500

    html redirect to get nightly

--- data/tar-repos/tar-gz-repos.sh
+++ data/tar-repos/tar-gz-repos.sh
@@ -2,14 +2,33 @@
 STATIC=/opt/phab/static/
 ACTIVE=/opt/phacility/git/
 ARCHIVED=/opt/phab/archived/git/dir/
+HTML='
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <script type="text/javascript">
+            window.location.href = "FILENAME"
+        </script>
+        <title>nightly build of FILENAME</title>
+    </head>
+    <body>
+    	you should be automatically redirected shortly...
+	</body>
+</html>
+'
 
 rm -f ${STATIC}/*.gz
+rm -f ${STATIC}/*.html
+TODAY=$(date +%Y%m%d)
 
 function tar-dir-files()
 {
     cd $1
     for f in $(ls); do
-        cd $1/$f && tar -zcf $STATIC/$f.gz *
+        file_name=$f$TODAY.gz
+        cd $1/$f && tar -zcf $STATIC/$file_name *
+        echo $HTML | sed "s/FILENAME/$file_name/g" > $STATIC/$file_name.html
     done
 }
 
