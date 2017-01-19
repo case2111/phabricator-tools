d32bccb8b66a2ad14dd22a7a8eac0653b9a19b9f
Sean Enck <enckse@gmail.com>
2017-01-19 11:12:49 -0500

    actually want the rw name for html output

--- data/tar-repos/tar-gz-repos.sh
+++ data/tar-repos/tar-gz-repos.sh
@@ -28,7 +28,7 @@ function tar-dir-files()
     for f in $(ls); do
         file_name=$f$TODAY
         cd $1/$f && tar -zcf $STATIC/$file_name.gz *
-        echo $HTML | sed "s/FILENAME/$file_name/g" > $STATIC/$file_name.html
+        echo $HTML | sed "s/FILENAME/$file_name/g" > $STATIC/$f.html
     done
 }
 
