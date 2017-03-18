120c9fff1eb417448b8331b1c787bc9424b1156b
Sean Enck <enckse@gmail.com>
2017-03-17 22:20:19 -0400

    actually any of this as well

--- utilities/system/scripts/timed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /etc/environment
-curl -s $PHAB_HOST/api/conpherence.updatethread \
-    -d api.token="$PHAB_STATUS_TOKEN" \
-    -d id=$PHAB_BOT_ROOM \
-    -d message="$PHAB_TIMED_MSG $1"
-rm -f /opt/phab/static/*.pdf
-rm -f /opt/phab/static/*.md
