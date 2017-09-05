7251d2f08c1bc88d0c9703afd3c8328732503099
Sean Enck <enckse@gmail.com>
2017-09-05 16:00:56 -0400

    zip controller should work with some tweaks

--- extensions/ZipController.php
+++ extensions/ZipController.php
@@ -6,11 +6,31 @@ final class ZipController extends PhabricatorController {
     if (!file_exists($full)) {
         return new Aphront404Response();
     }
+    $tmp = tempnam("/tmp", "repo-");
+    $zip = new ZipArchive();
+    $zip->open($tmp, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($full),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+
+    foreach ($files as $name => $file)
+    {
+        if (!$file->isDir())
+        {
+            $filePath = $file->getRealPath();
+            $relativePath = substr($filePath, strlen($full) + 1);
+            $zip->addFile($filePath, $relativePath);
+        }
+    }
+
+    $zip->close();
+
     $response = new AphrontFileResponse();
     $response->setMimeType('application/zip');
     $response->setCanCDN(false);
     $response->setDownload($file);
-    $handle = fopen($full, "rb");
+    $handle = fopen($tmp, "rb");
     if (!$handle) {
         return new Aphront404Response();
     }
