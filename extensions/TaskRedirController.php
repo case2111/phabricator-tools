a91a7d89ebd7b680aaca02d1c384677179bd9414
Sean Enck <enckse@gmail.com>
2017-12-28 16:53:54 -0500

    final cleanup

--- extensions/TaskRedirController.php
+++ extensions/TaskRedirController.php
@@ -7,19 +7,19 @@ final class TaskRedirController extends PhabricatorController {
         $maniphest = $path . 'forms.csv';
         $name = explode('/', $request->getRequestURI())[2];
         if (file_exists($maniphest) ) {
-            $csv = array_map('str_getcsv', file($maniphest));	
+            $csv = array_map('str_getcsv', file($maniphest));    
             $header = array_shift($csv);
             foreach ($csv as $value) {
                 if ($name == $value[0]) {
                     $use_file = $path . $value[0] . ".md";
                     if (file_exists($use_file)) {
-	                    $raw_text = file_get_contents($use_file);
-		    			$quoted = urlencode($raw_text);
-			    		$and = $value[1];
-				    	$response = new AphrontRedirectResponse();
-					    $response->setURI("/maniphest/task/edit/form/default/?description=" . $quoted . $and);
-    					return $response;
-	    		    }
+                        $raw_text = file_get_contents($use_file);
+                        $quoted = urlencode($raw_text);
+                        $and = $value[1];
+                        $response = new AphrontRedirectResponse();
+                        $response->setURI("/maniphest/task/edit/form/default/?description=" . $quoted . $and);
+                        return $response;
+                    }
                 }
             }
         }
