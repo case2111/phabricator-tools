178fa910e44a008c42aa1a8f9c363933c710dae2
Sean Enck <enckse@gmail.com>
2017-12-28 17:03:43 -0500

    mostly done with space changes

--- extensions/TaskRedirHostApplication.php
+++ extensions/TaskRedirHostApplication.php
@@ -2,29 +2,29 @@
 
 final class TaskRedirHostApplication extends PhabricatorApplication {
 
-  public function getName() {
-    return pht('Task Redirection Hosting');
-  }
+    public function getName() {
+        return pht('Task Redirection Hosting');
+    }
 
-  public function getShortDescription() {
-    return pht('Supports task redirection hosting within phabricator');
-  }
+    public function getShortDescription() {
+        return pht('Supports task redirection hosting within phabricator');
+    }
 
-  public function getBaseURI() {
-    return '/sfh/';
-  }
+    public function getBaseURI() {
+        return '/sfh/';
+    }
 
-  public function getRoutes() {
-    return array(
-      '/taskredir/([a-z]+)' => 'TaskRedirController'
-    );
-  }
+    public function getRoutes() {
+        return array(
+          '/taskredir/([a-z]+)' => 'TaskRedirController'
+        );
+    }
 
-  public function isUnlisted() {
-    return true;
-  }
+    public function isUnlisted() {
+        return true;
+    }
 
-  public function isLaunchabled() {
-    return false;
-  }
+    public function isLaunchabled() {
+        return false;
+    }
 }
--- extensions/ZipHostApplication.php
+++ extensions/ZipHostApplication.php
@@ -2,29 +2,29 @@
 
 final class ZipHostApplication extends PhabricatorApplication {
 
-  public function getName() {
-    return pht('Repository bundled zips');
-  }
+    public function getName() {
+        return pht('Repository bundled zips');
+    }
 
-  public function getShortDescription() {
-    return pht('Supports downloading repositories as zips');
-  }
+    public function getShortDescription() {
+        return pht('Supports downloading repositories as zips');
+    }
 
-  public function getBaseURI() {
-    return '/zip/';
-  }
+    public function getBaseURI() {
+        return '/zip/';
+    }
 
-  public function getRoutes() {
-    return array(
-      '/zip/(\d+)' => 'ZipController'
-    );
-  }
+    public function getRoutes() {
+        return array(
+          '/zip/(\d+)' => 'ZipController'
+        );
+    }
 
-  public function isUnlisted() {
-    return true;
-  }
+    public function isUnlisted() {
+        return true;
+    }
 
-  public function isLaunchabled() {
-    return false;
-  }
+    public function isLaunchabled() {
+        return false;
+    }
 }
