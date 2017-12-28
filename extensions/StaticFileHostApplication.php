799d18e66acc14447c70549b304ed72bcc6dc82a
Sean Enck <enckse@gmail.com>
2017-12-28 17:02:15 -0500

    Update static file for consistent line starts

--- extensions/StaticFileHostApplication.php
+++ extensions/StaticFileHostApplication.php
@@ -2,29 +2,29 @@
 
 final class StaticFileHostApplication extends PhabricatorApplication {
 
-  public function getName() {
-    return pht('Static File Hosting');
-  }
+    public function getName() {
+        return pht('Static File Hosting');
+    }
 
-  public function getShortDescription() {
-    return pht('Supports static file hosting within phabricator');
-  }
+    public function getShortDescription() {
+        return pht('Supports static file hosting within phabricator');
+    }
 
-  public function getBaseURI() {
-    return '/sfh/';
-  }
+    public function getBaseURI() {
+        return '/sfh/';
+    }
 
-  public function getRoutes() {
-    return array(
-      '/sfh/(\d+)\.([a-z]+)' => 'StaticFileController'
-    );
-  }
+    public function getRoutes() {
+        return array(
+          '/sfh/(\d+)\.([a-z]+)' => 'StaticFileController'
+        );
+    }
 
-  public function isUnlisted() {
-    return true;
-  }
+    public function isUnlisted() {
+        return true;
+    }
 
-  public function isLaunchabled() {
-    return false;
-  }
+    public function isLaunchabled() {
+        return false;
+    }
 }
