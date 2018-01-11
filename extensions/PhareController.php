803d11a90d2ce053b81948a9e676cd0cc644d403
Sean Enck <enckse@gmail.com>
2018-01-11 13:12:27 -0500

    Phare extensions

---
+++ extensions/PhareController.php
@@ -0,0 +1,26 @@
+<?php
+final class PhareController extends PhabricatorController {
+    public function handleRequest(AphrontRequest $request) {
+        $pharing = '/opt/phare/';
+        $user = strtolower($request->getUser()->getUserName());
+        $name = strtolower(explode('/', $request->getRequestURI())[2]);
+        $file_name = $pharing . $user . "." . $name . "*";
+        foreach (glob($file_name) as $f_name) {
+            $response = new AphrontFileResponse();
+            $response->setMimeType("application/octet-stream");
+            $response->setCanCDN(false);
+            $full_name = $f_name;
+            $handle = fopen($full_name, 'rb');
+            if (!$handle) {
+                continue;
+            }
+            $base = basename($full_name);
+            $response->setDownload($base);
+            $contents = fread($handle, filesize($full_name));
+            fclose($handle);
+            $response->setContent($contents);
+            return $response;
+        }
+        return new Aphront404Response();
+    }
+}
