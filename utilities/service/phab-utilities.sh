86ab20f9ee6281e275bea958f080506dd47626ba
Sean Enck <enckse@gmail.com>
2018-03-18 16:06:38 -0400

    renaming

--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BIN=/usr/bin/phabricator-utils-
+BIN=/usr/bin/phab-utilities-
 source /etc/epiphyte.d/environment
 for e in $(echo "projects tasks wiki"); do
     $BIN$e | grep "ERROR" | systemd-cat -p err
