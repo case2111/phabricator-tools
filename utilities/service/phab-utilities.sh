41178a1c95bc3f1a3b8abaa435daf988eea2dc05
Sean Enck <enckse@gmail.com>
2018-03-19 10:42:26 -0400

    send to smirc

--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -8,6 +8,6 @@ if [ $dayofweek -eq 7 ]; then
     cmds="$cmds $tasks"
 fi
 for e in $(echo "$cmds"); do
-    $BIN$e --env /etc/epiphyte.d/environment 2>&1 | grep -v "INFO" | systemd-cat -p err -t phab-utils
+    $BIN$e --env /etc/epiphyte.d/environment 2>&1 | grep -v "INFO" | smirc
 done
 curl $SYNAPSE_LOCAL_URL/shutdown
