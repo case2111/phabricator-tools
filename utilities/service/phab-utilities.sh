519cf22655136796a66f65430f834f8bece48959
Sean Enck <enckse@gmail.com>
2018-03-19 10:11:04 -0400

    add weekly back into the mix

--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 BIN=/usr/bin/phab-utilities-
 source /etc/epiphyte.d/environment
-for e in $(echo "projects tasks wiki"); do
+cmds="projects wiki"
+dayofweek=$(date +%u)
+if [ $dayofweek -eq 7 ]; then
+    cmds="$cmds $tasks"
+fi
+for e in $(echo "$cmds"); do
     $BIN$e --env /etc/epiphyte.d/environment 2>&1 | grep -v "INFO" | systemd-cat -p err -t phab-utils
 done
 curl $SYNAPSE_LOCAL_URL/shutdown
