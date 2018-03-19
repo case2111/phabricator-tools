1737ae4bcda47d41b061b7b559cf683192001867
Sean Enck <enckse@gmail.com>
2018-03-19 09:50:58 -0400

    redir stderr

--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -2,6 +2,6 @@
 BIN=/usr/bin/phab-utilities-
 source /etc/epiphyte.d/environment
 for e in $(echo "projects tasks wiki"); do
-    $BIN$e --env /etc/epiphyte.d/environment | grep -v "INFO" | systemd-cat -p err
+    $BIN$e --env /etc/epiphyte.d/environment 2>&1 | grep -v "INFO" | systemd-cat -p err
 done
 curl $SYNAPSE_LOCAL_URL/shutdown
