a42e95b7b61466560db1b7dc0e074aaec8e9d86f
Sean Enck <enckse@gmail.com>
2018-03-19 10:11:51 -0400

    weekly task output

--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -4,6 +4,7 @@ source /etc/epiphyte.d/environment
 cmds="projects wiki"
 dayofweek=$(date +%u)
 if [ $dayofweek -eq 7 ]; then
+    echo "weekly tasks..."
     cmds="$cmds $tasks"
 fi
 for e in $(echo "$cmds"); do
