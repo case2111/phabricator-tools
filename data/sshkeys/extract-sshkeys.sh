11c8953ce6aa7427f47cf4080f3f1518634c9201
Sean Enck <enckse@gmail.com>
2018-03-20 10:57:21 -0400

    initial rev on extracting keys

---
+++ data/sshkeys/extract-sshkeys.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+WORKDIR="/opt/phab/sshkeys"
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+
+source /etc/epiphyte.d/environment
+json=$(curl -s $PHAB_HOST/api/user.search \
+       -d api.token=$SYNAPSE_PHAB_TOKEN \
+       -d queryKey=active)
+users=$(echo "$json" | python -c "
+import sys
+import json
+
+inputs = sys.stdin.read()
+j = json.loads(inputs)
+for u in j['result']['data']:
+	print(u['phid'] + ':' + u['fields']['username'])
+")
+
+if [ $? -ne 0 ]; then
+	echo "unable to retrieve users"
+	exit 1
+fi
+
+for u in $(echo $users); do
+	name=$(echo $u | cut -d ":" -f 2)
+	phid=$(echo $u | cut -d ":" -f 1)
+	p=$(curl -s $PHAB_HOST/api/auth.authorizedkeys \
+	         -d api.token=$SYNAPSE_PHAB_TOKEN \
+	         -d phids[0]=$phid)
+	keys=$(echo $p | python -c "
+import sys
+import json
+inputs = sys.stdin.read()
+j = json.loads(inputs)
+for k in j['result']['data']:
+	print(k)
+")
+    echo "user: $name ($phid)"
+    if [ $? -ne 0 ]; then
+        echo "error getting keys"
+        continue
+    fi
+	if [ -z "$keys" ]; then
+		echo "no keys"
+		continue
+	fi
+	echo "found keys: $name"
+	path=$WORKDIR/$name
+	echo "$keys" > $path.keys
+done
+
+chmod 666 -R $WORKDIR
