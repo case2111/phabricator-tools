14e9d764e19125b6a6a6d7314b6b8283a55b234c
Sean Enck <enckse@gmail.com>
2018-03-21 11:48:59 -0400

    fixing output

--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -5,10 +5,10 @@ cmds="projects wiki"
 dayofweek=$(date +%u)
 if [ $dayofweek -eq 7 ]; then
     echo "weekly tasks..."
-    cmds="$cmds $tasks"
+    cmds="$cmds tasks"
 fi
 for e in $(echo "$cmds"); do
     $BIN$e --env /etc/epiphyte.d/environment 2>&1 | grep -v "INFO" | smirc
 done
 curl $SYNAPSE_LOCAL_URL/shutdown
-echo "done: $cmds" | smirc
+echo "done: "$(echo $cmds | sed "s/ /,/g") | smirc
