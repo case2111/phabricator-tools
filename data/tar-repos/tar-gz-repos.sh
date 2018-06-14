991986fa2f3bec4a079caff4f5c4bc2cadd10f20
Sean Enck <enckse@gmail.com>
2018-06-14 09:03:53 -0400

    stop doing this

--- data/tar-repos/tar-gz-repos.sh
+++ data/tar-repos/tar-gz-repos.sh
@@ -2,19 +2,6 @@
 STATIC=/opt/phab/static/
 ACTIVE=/opt/phacility/git/
 ARCHIVED=/opt/phab/archived/git/dir/
-HTML='
-<!DOCTYPE HTML>
-<html lang="en-US">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" contents="5;url=FILENAME" />
-        <title>nightly build of FILENAME</title>
-    </head>
-    <body>
-    you should be automatically redirected shortly...(or click <a href="FILENAME">here</a>)
-	</body>
-</html>
-'
 
 rm -f ${STATIC}/*.gz
 rm -f ${STATIC}/*.html
@@ -26,7 +13,6 @@ function tar-dir-files()
     for f in $(ls); do
         file_name=$f$TODAY.gz
         cd $1/$f && tar -zcf $STATIC/$file_name *
-        echo $HTML | sed "s/FILENAME/$file_name/g" > $STATIC/$f.html
     done
 }
 
