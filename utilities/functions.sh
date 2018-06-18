d7699573a6cc272137f543f8151316f3e25086fa
Sean Enck <enckse@gmail.com>
2018-06-18 16:29:08 -0400

    debugging ability

--- utilities/functions.sh
+++ utilities/functions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+INFO_MODE="[INFO]"
 function phabricator_encode() {
     _py="
 import sys
@@ -6,4 +7,8 @@ import urllib.parse
 
 print(urllib.parse.quote(sys.stdin.read()))"
     echo "$@" | python -c "$_py"
+}
+
+function info_mode() {
+    echo "$INFO_MODE $@"
 }
--- utilities/service/phab-utilities.sh
+++ utilities/service/phab-utilities.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BIN=/usr/bin/phab-utilities-
 source /etc/epiphyte.d/environment
+source /usr/share/phabricator-tools/functions.sh
 cmds="projects wiki"
 dayofweek=$(date +%u)
 if [ $dayofweek -eq 7 ]; then
@@ -8,7 +9,7 @@ if [ $dayofweek -eq 7 ]; then
     cmds="$cmds tasks"
 fi
 for e in $(echo "$cmds"); do
-    $BIN$e | smirc
+    $BIN$e | grep -v "$INFO_MODE" | smirc
 done
 curl $SYNAPSE_LOCAL_URL/shutdown
 echo "done: "$(echo $cmds | sed "s/ /,/g") | smirc --report
