e845cbe84d9327dd90ccf6783f3bb9d0d2802531
Sean Enck <enckse@gmail.com>
2018-06-18 16:15:12 -0400

    converting wiki

---
+++ utilities/wiki.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source /etc/epiphyte.d/environment
+DASH=$(echo "$PHAB_TO_DASH" | cut -d "," -f 1)
+WIKI=$(echo "$PHAB_TO_DASH" | cut -d "," -f 2)
+results=$(curl -s $PHAB_HOST/api/phriction.document.search \
+            -d api.token=$PHAB_TOKEN \
+            -d constraints[phids][0]=$WIKI \
+            -d attachments[content]=1)
+if [ -z "$results" ]; then
+    echo "unable to get wiki content"
+    exit 1
+fi
+
+function _encode() {
+    _py="
+import sys
+import urllib.parse
+
+print(urllib.parse.quote(sys.stdin.read()))"
+    echo "$@" | python -c "$_py"
+}
+
+_segment="
+import sys
+import json
+
+print(json.loads(sys.stdin.read())['result']['data'][0]['attachments']['content']['content']['raw'])"
+results=$(echo "$results" | python -c "$_segment")
+curl -s $PHAB_HOST/api/dashboard.panel.edit \
+        -d api.token=$PHAB_TOKEN \
+        -d objectIdentifier=$DASH \
+        -d transactions[0][type]="custom.text" \
+        -d transactions[0][value]=$(_encode "$results")
