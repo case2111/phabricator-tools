1593d98a23c00fbba8c632ecd765494ae18f8c94
Sean Enck <enckse@gmail.com>
2018-06-18 19:33:11 -0400

    cleaning up usage

--- utilities/tasks.sh
+++ utilities/tasks.sh
@@ -2,6 +2,9 @@
 source /etc/epiphyte.d/environment
 source /usr/share/phabricator-tools/functions.sh
 HIDDEN_TASKS=${CACHE}hiddentasks
+TMP_FILE=${CACHE}taskcache
+
+IDX="index,"
 _hiddentasks() {
     info_mode "tracking hidden tasks"
     file=$HIDDEN_TASKS
@@ -31,44 +34,62 @@ _hiddentasks() {
     echo "$next" > $file
 }
 
-_unmodified() {
-    COMMENT=$(phabricator_encode "task updated due to inactivity")
-    PROJECTID=$(echo "$PHAB_UNMODIFIED" | cut -d "," -f 1)
-    MONTHS=$(echo "$PHAB_UNMODIFIED" | cut -d "," -f 2)
+_recalc() {
     max=$(cat $HIDDEN_TASKS)
-    old=$(date -d "$MONTHS months ago" +%s)
     _pymod="
 import sys
 import json
 r = json.loads(sys.stdin.read())
 o = r['result']['data'][0]
-print('{},{}'.format(o['fields']['dateModified'],o['phid']))"
+f = o['fields']
+i = None
+c = o['fields']
+if 'custom.custom:index' in c:
+    i = c['custom.custom:index']
+print('{},{},{},{},{}'.format(f['dateModified'],o['phid'],f['ownerPHID'], i, f['status']['value']))"
     for s in $(seq 0 $max); do
         result=$(curl -s $PHAB_HOST/api/maniphest.search \
                     -d api.token=$PHAB_TOKEN \
                     -d queryKey=all \
-                    -d constraints[statuses][0]=open \
-                    -d constraints[statuses][1]=actionneeded \
                     -d constraints[ids][0]=$s)
         if [ ! -z "$result" ]; then
             echo "$result" | grep -q "PHID-TASK"
             if [ $? -eq 0 ]; then
                 item=$(echo "$result" | python -c "$_pymod")
-                mod=$(echo "$item" | cut -d "," -f 1)
-                id=$(echo "$item" | cut -d "," -f 2)
-                if [ $mod -lt $old ]; then
-                    curl -s $PHAB_HOST/api/maniphest.edit \
-                        -d api.token=$PHAB_TOKEN \
-                        -d objectIdentifier=$id \
-                        -d transactions[0][type]="comment" \
-                        -d transactions[0][value]=$COMMENT \
-                        -d transactions[1][type]="projects.add" \
-                        -d transactions[1][value][]=$PROJECTID
-                fi
+                item="$IDX$s,$item"
+                echo "$item" >> $TMP_FILE
             fi
         fi
     done
 }
 
-_hiddentasks
+_unmodified() {
+    COMMENT=$(phabricator_encode "task updated due to inactivity")
+    PROJECTID=$(echo "$PHAB_UNMODIFIED" | cut -d "," -f 1)
+    MONTHS=$(echo "$PHAB_UNMODIFIED" | cut -d "," -f 2)
+    old=$(date -d "$MONTHS months ago" +%s)
+    for r in $(cat $TMP_FILE | grep -E "(open|actionneeded)$"); do
+        mod=$(echo "$r" | cut -d "," -f 3)
+        id=$(echo "$r" | cut -d "," -f 4)
+        user=$(echo "$r" | cut -d "," -f 5)
+        echo "$user" | grep -q "PHID-USER";
+        if [ $? -eq 0 ]; then
+            continue
+        fi
+        if [ $mod -lt $old ]; then
+            curl -s $PHAB_HOST/api/maniphest.edit \
+                -d api.token=$PHAB_TOKEN \
+                -d objectIdentifier=$id \
+                -d transactions[0][type]="comment" \
+                -d transactions[0][value]=$COMMENT \
+                -d transactions[1][type]="projects.add" \
+                -d transactions[1][value][]=$PROJECTID
+        fi
+    done
+}
+
+info_mode "$TMP_FILE"
+rm -f $TMP_FILE
+_recalc
+_hiddentask
 _unmodified
