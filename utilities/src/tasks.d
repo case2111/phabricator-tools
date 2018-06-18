75118c63138f7bb42091bd40b2e8abe0b09ee27b
Sean Enck <enckse@gmail.com>
2018-06-18 17:16:56 -0400

    cleaning up source

--- utilities/src/tasks.d
+++ utilities/src/tasks.d
@@ -15,73 +15,6 @@ import std.string: format, join, split, strip;
 import std.stdio: writeln;
 
 /**
- * Hidden task checking
- */
-private static int hiddenTasks(TaskCtx ctx, int start, int page)
-{
-    auto show = "none found";
-    int last = -1;
-    if (page < 1 || start < 1)
-    {
-        show = "please select a bounded start (> 0) and page (> 0)";
-    }
-    else
-    {
-        auto results = restricted(ctx.settings, start, page);
-        if (results.length > 0)
-        {
-            show = "T" ~ join(results, ", T");
-            foreach (result; results)
-            {
-                auto id = to!int(result);
-                if (last == -1 || id < last)
-                {
-                    last = id;
-                }
-            }
-        }
-    }
-
-    return last;
-}
-
-/**
- * Scheduled hidden task checking
- */
-private static bool doHiddenTasks(TaskCtx ctx)
-{
-    try
-    {
-        auto lastStart = 1;
-        auto opts = ctx.api.context[HiddenOpts].split(",");
-        auto phid = opts[0];
-        auto room = opts[1];
-        auto paste = construct!PasteAPI(ctx.settings);
-        auto contents = paste.activeByPHID(phid, true)[ResultKey][DataKey];
-        auto attached = contents.array[0]["attachments"]["content"]["content"];
-        auto text = attached.str.split("=");
-        lastStart = to!int(text[1].strip());
-        auto result = hiddenTasks(ctx, lastStart, 500);
-        if (result != lastStart && result >= 0)
-        {
-            auto today = Clock.currTime().dayOfYear();
-            paste.editText(phid, format("%s=%s", today, result));
-        }
-        else
-        {
-            writeln(format("task visibility index unchanged: T%s", result));
-        }
-
-        return true;
-    }
-    catch (Exception e)
-    {
-        writeln(e);
-        return false;
-    }
-}
-
-/**
  * Unmodified operations
  */
 private static bool tasksUnmodified(TaskCtx ctx)
@@ -110,48 +43,7 @@ void main(string[] args)
     {
         writeln("unmod tasks");
     }
-    if (!doHiddenTasks(ctx))
-    {
-        writeln("hidden tasks");
-    }
     info("tasks");
-}
-
-// Id field for tasks
-private enum IdField = "id";
-
-/**
- * Get a task id e.g. T[0-9]+
- */
-private static string getId(JSONValue task)
-{
-    return "T" ~ to!string(task[IdField].integer);
-}
-
-/**
- * Move all tasks from a query to a project
- */
-private static int queryToProject(Settings settings,
-                                   string query,
-                                   string projectPHID)
-{
-    int count = 0;
-    try
-    {
-        auto maniphest = construct!ManiphestAPI(settings);
-        auto queried = maniphest.byQueryKey(query)[ResultKey][DataKey];
-        foreach (task; queried.array)
-        {
-            maniphest.addProject(task[PHID].str, projectPHID);
-            count++;
-        }
-    }
-    catch (Exception e)
-    {
-        count = count * -1;
-    }
-
-    return count;
 }
 
 /**
@@ -201,63 +93,5 @@ private static bool unmodified(Settings settings,
     {
         writeln(e);
         return false;
-    }
-}
-
-/**
- * Detects restricted tasks
- */
-private static string[] restricted(Settings settings, int start, int page)
-{
-    try
-    {
-        if (page < 0)
-        {
-            return [];
-        }
-
-        int paging = 0;
-        int[] ids;
-        bool[long] matched;
-        while (paging <= page)
-        {
-            ids ~= start + paging;
-            matched[start + paging] = false;
-            paging++;
-        }
-
-        auto maniphest = construct!ManiphestAPI(settings);
-        auto raw = maniphest.byIds(ids);
-        auto all = raw[ResultKey][DataKey];
-        foreach (task; all.array)
-        {
-            if (FieldsKey in task)
-            {
-                auto id = task[IdField].integer;
-                if (id in matched)
-                {
-                    matched[id] = true;
-                }
-                else
-                {
-                    matched[id] = false;
-                }
-            }
-        }
-
-        string[] results;
-        foreach (match; matched.keys.sort!())
-        {
-            if (!matched[match])
-            {
-                results ~= to!string(match);
-            }
-        }
-
-        return results;
-    }
-    catch (Exception e)
-    {
-        return [];
     }
 }
